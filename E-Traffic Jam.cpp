f79dfad0e9afd9037931900fc3bea3cd44297ee4
Sorowar Mahabub <76944781+sorowar-cse@users.noreply.github.com>
2022-12-03 20:24:30 +0600

    Update E-Traffic Jam.cpp

--- E-Traffic Jam.cpp
+++ E-Traffic Jam.cpp
@@ -1,1 +1,13 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    double n,b,a;
+    int t;
+    cin>>t;
+    while(t--){
+    cin>>a>>b;
+    n=sqrt(a*a+b*b);
+    double ans=(a*b)/n;
+    cout<<fixed<<setprecision(2)<<ans<<endl;}
+    return 0;
+}
